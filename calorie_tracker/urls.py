b7dc5e2ea01e91776b9935def6655419f5bcda71
denneb-prosolutions <denneb.prosolutions@gmail.com>
2022-03-25 11:28:07 +0800

    added layout design

--- calorie_tracker/urls.py
+++ calorie_tracker/urls.py
@@ -1,4 +1,4 @@
-"""calorie_counter URL Configuration
+"""calorie_tracker URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.0/topics/http/urls/
@@ -14,8 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
+from home import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('accounts.urls')),
+    path('',views.denneb_home)
 ]
--- calorie_tracker/wsgi.py
+++ calorie_tracker/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for calorie_counter project.
+WSGI config for calorie_tracker project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'calorie_counter.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'calorie_tracker.settings')
 
 application = get_wsgi_application()
